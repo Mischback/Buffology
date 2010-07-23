86a7cf9bf82911a180dc8a92d9fe110e18472fdd
Mischback <spamdead.mb@gmail.com>
2010-07-24 00:55:45 +0200

    Working on the Configuration-Frame.
    Created SlashCmd, the basic frame and the configuration-tabs.

--- menu.lua
+++ menu.lua
@@ -19,6 +19,36 @@ local menu = CreateFrame('Frame')						-- create the menu
 		end
 		menu.MenuFrame:Hide()							-- Hide the menu
 	end
+
+	--[[
+	
+	]]
+	function Buffology_Frames()
+		_G['Buffology_MenuTabFramesCaption']:SetText('Frames')
+	end
+
+	--[[
+	
+	]]
+	function Buffology_Filters()
+		_G['Buffology_MenuTabFiltersCaption']:SetText('Filters')
+	end
+
+	--[[
+	
+	]]
+	function Buffology_TabFramesButton()
+		_G['Buffology_MenuTabFrames']:Show()
+		_G['Buffology_MenuTabFilters']:Hide()
+	end
+
+	--[[
+	
+	]]
+	function Buffology_TabFiltersButton()
+		_G['Buffology_MenuTabFrames']:Hide()
+		_G['Buffology_MenuTabFilters']:Show()
+	end
 	
 -- *****************************************************
 
